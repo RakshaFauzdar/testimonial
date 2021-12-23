481fb5176dc6b430ed843af895dd50ee66e2f5ce
RakshaFauzdar <raksha.fauzdar@gmail.com>
2021-12-23 12:19:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Testimonial from "./Testimonial";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Carousel />
+      <MultiItemCarousel />
+      <BootstrapMultiCarousel /> */}
+      <Testimonial />
+    </div>
+  );
+}
+
+export default App;
